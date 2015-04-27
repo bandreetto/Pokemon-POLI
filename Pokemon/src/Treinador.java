8119ca612351990668ed98b8f9a0e359ac62794c
mac0321 41 <mac0321.41@hulk.cec.ime.usp.br>
2015-04-27 10:18:56 -0300

    Alteracao treinador

--- Pokemon/src/Treinador.java
+++ Pokemon/src/Treinador.java
@@ -1,8 +1,50 @@
 
+import java.util.ArrayList;
 public class Treinador {
 
-	public Treinador() {
-		// TODO Auto-generated constructor stub
+	private int numero;
+	private String nome;
+	private ArrayList<Pokemon> time_pokemons = new ArrayList<Pokemon>();
+	private Pokemon ativo;
+	
+	public Treinador(String nom, int num){
+		int i;
+		nome = nom;
+		numero = num;
+		montarTimePokemon();
+		ativo = time_pokemons[0];
+		
+		
+	} 
+	//classe para monta time pokemon
+	public void montarTimePokemon(){
+		for(i = 0; i< 6; i++){
+			time_pokemons[i] = 
+		}
+	}
+	
+	public void selecionaPokAtivo(){
+		
+	}
+	
+	//troca o Pokemon ativo pelo escolhido
+	public void trocarPokemon(Pokemon [] time; Pokemon x){
+		// o pokemon escolhido tem que estar no time!
+		ativo = x;
+		
+	}
+	
+	public void fugir(){
+		
+		
+	}
+	public void
+	public void usarItem(Item item){
+		
+	}
+	
+	public void atacar(){
+		
 	}
 
-}wdfgwer
+}
