338bd5c1988ade8403949bc56339d7f71fe6f85c
Bruno <bandreetto>
2015-04-30 10:55:31 -0300

    Finalizando Charmander

--- Pokemon/src/Charmander.java
+++ Pokemon/src/Charmander.java
@@ -1,13 +1,13 @@
 
 public class Charmander extends Pokemon{
 	public static final int id = 1;
-	public static final int FOR = 30, AGI = 9, DEF = 100;
+	private final int FOR = 3, AGI = 9, DEF = 10;
 	private final String tipo = "Fogo";
 	private int hp;
 	
-	
-	public Charmander() {
-		hp = 1000;
+	// cria um charmander com 100 de hp
+	public Charmander() { 
+		hp = 100;
 	}
 	
 	public int getFOR() {
@@ -22,18 +22,42 @@ public class Charmander extends Pokemon{
 		return DEF;
 	}
 	
+	// a seguir os ataques de charmander já respeitando fraquezas e vantagens
+	// Fórmula para dano = FOR*ATQ BASE*FRAQUESA/RESISTÊNCIA - DEFESA ALVO
 	public void Brasas(Pokemon alvo) {
 		if (alvo.getTipo() == "Grama") {
 			alvo.dano((int)(FOR*15*1.5 - alvo.getDEF()));
 		} else if (alvo.getTipo() == "Agua" || alvo.getTipo() == "Pedra" || alvo.getTipo() == "Fogo") {
 			alvo.dano((int)(FOR*15*0.5 - alvo.getDEF()));
 		} else {
-			alvo.dano((int)(FOR*15 - alvo.getDEF()));
+			alvo.dano((FOR*15 - alvo.getDEF()));
 		}
 	}
 	
-	public void Arranhão(Pokemon alvo) {
-		alvo.dano((int)(FOR*10 - alvo.getDEF()));
+	public void Arranhao(Pokemon alvo) {
+		if (alvo.getTipo() == "Pedra") {
+			alvo.dano((int)(FOR*10*0.5 - alvo.getDEF())); 
+		} else {
+			alvo.dano((FOR*10 - alvo.getDEF()));
+		}
 	}
-
+	
+	public void GarradeMetal(Pokemon alvo) {
+		if (alvo.getTipo() == "Pedra") {
+			alvo.dano((int)(FOR*15*1.5 - alvo.getDEF()));
+		} else if (alvo.getTipo() == "Fogo" || alvo.getTipo() == "Agua" || alvo.getTipo() == "Eletrico") {
+			alvo.dano((int)(FOR*15*0.5 - alvo.getDEF()));
+		} else {
+			alvo.dano(FOR*15 - alvo.getDEF());
+		}
+	}
+	
+	// A furia depende do hp restante do pokemon, quanto menor o hp maior o dano
+	public void Furia(Pokemon alvo) {
+		if (alvo.getTipo() == "Pedra") {
+			alvo.dano((int)((1/hp)*(FOR*30*0.5 - alvo.getDEF())));
+		} else {
+			alvo.dano((int)(1/hp)*(FOR*30 - alvo.getDEF()));
+		}
+	}
 }
